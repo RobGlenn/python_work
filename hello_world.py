5874570804ad5a3a73ed989532f96320b7736e6b
RobGlenn <robert.glenn.357@gmail.com>
2016-06-23 19:40:37 -0700

    2nd commit

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 message = 'Hello Python World!!!!!'
 
 print(message)
 
 print ("Yet Another Commit")
 
-=======
-message = 'Hello Python World!!!!!'
 
-print(message)
-
-print ("Yet Another Commit")
-
->>>>>>> f4218b738fb7b75194ed51da70ef7fc0e2d255b0
